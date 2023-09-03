c079fde7cca620019c692623ea369d6b081fd869
gitdenlev <levchenkoden02@gmail.com>
2023-09-03 20:53:28 +0300

    Fix bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/dream-cakes/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
